1bf2a1a8d4538f90c65dce29e026e2bedce93514
Trần Hữu Cương <cuong.9312@gmail.com>
2021-05-27 11:29:28 +0700

    add code

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+class Demo {
+    message: string;
+  
+    constructor(message: string){
+      this.message = message;
+    }
+    
+    public hello() {
+      console.log(this.message);
+    }
+  }
+  
+  const demo = new Demo('hello world');
+  demo.hello();
+  
